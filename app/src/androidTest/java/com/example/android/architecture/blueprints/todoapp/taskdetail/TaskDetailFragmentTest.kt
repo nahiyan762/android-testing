811fe0e4e32f9063420c7b109fd8019d459eca57
Mohammad Sultan Al Nahiyan <nahiyan762@users.noreply.github.com>
2022-05-31 23:29:02 +0600

    Done Service locator test

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,44 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class TaskDetailFragmentTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTasksRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun cleanupDb() = runBlocking {
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun activeTaskDetails_DisplayedInUi() = runBlocking {
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
+        repository.saveTask(activeTask)
+
+        // WHEN - Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(2000)
+    }
+}
